aa75ccac34b95a34f0e2d608dc2059b0682fbc70
lu <lu.anchoverri@gmail.com>
2022-09-05 19:15:00 -0300

    uso la consola soy re kpa

--- AL/src/Main.java
+++ AL/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Holissssssssssssssssssss");
+        System.out.println("Holisssssssssssssssssdskfjidsagfjradsigfeikrsoafgoikeragoiksss");
     }
 }
