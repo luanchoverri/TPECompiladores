186e0329fa5321d56f3c440d08bf29a7939acb2d
lu <lu.anchoverri@gmail.com>
2022-09-29 19:34:07 -0300

    cambios en matriz de acciones semnaticas y analizador lexico

--- src/AnalizadorLexico/AnalizadorLexico.java
+++ src/AnalizadorLexico/AnalizadorLexico.java
@@ -14,8 +14,8 @@
  
  public class AnalizadorLexico {
     
-    // -- CONSTANTES -- // 
-    
+    // -- CONSTANTES -- //
+
     public static final int ESTADOS = 18;       // Cantidad de estados del autómata.
     public static final int SIMBOLOS = 23;      // Cantidad de símbolos aceptados por el compilador.
     
@@ -25,66 +25,21 @@
     private Integer idToken; // id token obtenido del buffer
     private String archivoEntrada; // Variable para la lectura del archivo txt
     private TablaSimbolos tablaSimbolos;
+    private ArrayList <Atributo> tokensReconocidos; // Va procesando el codigo y acá guarda los tokens reconocidos
     private ArrayList <String> errores; // ArrayList para almacenar los errores encontrados junto con el numero de linea (Implementar?) !!!!!!!!!!!!!!
     
     // -- MATRICES -- // 
     private MatrizAccionesSemanticas matrizAccionesSemanticas;  // Matriz de acciones semánticas.
-    
+    private MatrizEstados matrizEstados;
     
     // -- Constructor --
     
     public AnalizadorLexico(String archivoEntrada){
         this.archivoEntrada = archivoEntrada;
-        this.matrizAccionesSemanticas = new MatrizAccionesSemanticas(ESTADOS, SIMBOLOS);
-        
-        // -- Acciones semanticas SIMPLES --
-        
-        // AS1 -> Inicializa el buffer
-        AccionSemanticaSimple AS1 = new InicializarBuffer(this);
-        
-        // AS7 -> Agrega char leido al String (Buffer)
-        AccionSemanticaSimple AS7 = new AgregarChar(this);
-        
-        // AS3 -> Devuelve el ID del Token de los simbolos (operadores aritmeticos, comparadores, etc)
-        AccionSemanticaSimple AS3 = new ObtenerIdToken(this, this.getTablaSimbolos());
-        
-        // AS8 -> Controla si es palabra reservada
-        AccionSemanticaSimple AS8 = new ChequearPalabraReservada(this, this.getTablaSimbolos());    
-        
-        // AS10 -> Controla el rango de los enteros. Si esta en rango, lo agrega a la tabla de simbolos, sino, devuelve error (catch)
-        AccionSemanticaSimple AS10 = new RangoEntero(this, this.getTablaSimbolos());
-        
-        // AS11 -> Controla el rango de los flotantes. Si esta en rango, lo agrega a la tabla de simbolos, sino, devuelve error (catch)
-        AccionSemanticaSimple AS11 = new RangoFlotante(this, this.getTablaSimbolos());
-        
-        // -- Acciones semanticas COMPUESTAS -- 
-        
-        
-        // ----- MATRIZ DE ACCIONES SEMÁNTICAS ------ //
-        
-        AccionSemantica [][] matrizSemantica = {
-            /*            L       d     .     _     S     <     >     =     &     |     +     -     *     /     %    \n     ;     :     ,     (     )   otro  Bl,Tab  $                  
-            /*0*/        {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null, null,  null,  null,  null,  null,  null,  null, null,  null,  null },
-            /*1*/        {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*2*/        {null,  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,  null},
-            /*3*/        {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*4*/        {null,  null, null, null, null, null, null, null, null, null,  null,  null, null, null, null, null, null, null, null, null, null, null, null,  null},
-            /*5*/        {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*6*/        {null,  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,  null},
-            /*7*/        {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*8*/        {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*9*/        {null, null, null, null, null, null, null, null,  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,  null},
-            /*10*/       {null, null, null, null, null, null, null, null, null,  null, null, null, null, null, null, null, null, null, null, null, null, null, null,  null},
-            /*11*/       {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*12*/       {null ,  null,  null, null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*13*/       {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*14*/       {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null, null, null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*15*/       {null ,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null,  null, null, null,  null,  null,  null,  null,  null,  null,  null,  null },
-            /*16*/       {null, null, null, null, null, null, null, null, null, null,  null, null, null, null, null, null, null, null, null, null, null, null,  null,  null},
-            /*17*/       {null ,  null, null,  null,  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,  null},
-            /*18*/       {null, null, null, null, null, null, null,  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,  null},   
-        };
-        this.matrizAccionesSemanticas.setMatrizAccionSemantica(matrizSemantica);                   
+        this.matrizAccionesSemanticas = new MatrizAccionesSemanticas();
+        this.matrizEstados = new MatrizEstados();
+
+
     }
     
     
@@ -118,7 +73,7 @@
     }
     
     public ArrayList<Atributo> getListaTokens() {
-        return null;
+        return this.tokensReconocidos;
     }
     
     public void imprimirErrores() {
--- src/AnalizadorLexico/MatrizEstados.java
+++ src/AnalizadorLexico/MatrizEstados.java
@@ -1,79 +1,79 @@
-//package AnalizadorLexico;
-//
-//public class MatrizEstados {
-//
-//
-//
-//    int[][] matriz = {    // Estado final =  0
-//        //		   L    d   .   _   F   <   >   =   +   -   *   /   '   \n   ;   :   ,  (    )   {   }  ot   !  BL  $
-//        // 		   0    1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24
-//        /*0*/    { 6,   1,  9, -1,  6,  3,  2,  5,  0,  0,  0,  0,  7,  0,  0,  0,  0,  0,  0,  0,  0, -1, -1,  0,  0  },
-//        /*1*/    { 0,   1, 10,  0,  0,  0,  0,  0,  0,	0,  0,  0,  0,	0,  0,  0,  0,	0,  0,  0,  0,	0,  0,  0,  0  },
-//        /*2*/    { 0,   0,	0,  0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
-//        /*3*/    { 0,   0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
-//        /*4*/    { 4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	0,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4  },
-//        /*5*/    { 0,   0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
-//        /*6*/    { 6,	6,	0,	6,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
-//        /*7*/    { 7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	8,	0,	p,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7  },
-//        /*8*/    { 7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	8,	0,	8,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7  },
-//        /*9*/    {-1,  10, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  },
-//        /*10*/   { 0,  10,  0,	0, 11,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
-//        /*11*/   {-1,  13, -1, -1, -1, -1, -1, -1, 12, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  },
-//        /*12*/   {-1,  13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  },
-//        /*13*/   { 0,  13,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  }
-//
-//
-//    };
-//
-//
-//    public int getEstado(int estado, int simbolo){
-//        return matriz[estado][simbolo];
-//    }
-//
-//    public int getColCaracter(Character caracter){
-//
-//        if (caracter == null)
-//            return -1;
-//
-//        if (caracter == 'F')
-//            return 4;
-//
-//        if (Character.isLetter(caracter))
-//            return 0;
-//
-//        if (Character.isDigit(caracter))
-//            return 1;
-//
-//        switch (caracter) {
-//            case '.': return 2;
-//            case '_': return 3;
-//            case '<': return 5;
-//            case '>': return 6;
-//            case '=': return 7;
-//            case '+': return 8;
-//            case '-': return 9;
-//            case '*': return 10;
-//            case '/': return 11;
-//            case 39 : return 12;   // comilla simple (ASCII)
-//            case '\n': return 13;
-//            case ';': return 14;
-//            case ':': return 15;
-//            case ',': return 16;
-//            case '(': return 17;
-//            case ')': return 18;
-//            case '{': return 19;
-//            case '}': return 20;
-//            case '!': return 22;
-//            case 9: return 23;      // Tabulación (ASCII)
-//            case 32: return 23;     // Espacio en blanco (ASCII)
-//            case '$': return 24;
-//        }
-//
-//        return 21; //Otros simbolos
-//
-//    }
-//
-//
-//
-//
-//}
+package AnalizadorLexico;
+
+public class MatrizEstados {
+
+    private int[][] matriz;
+
+    public MatrizEstados(){
+        this.matriz = {
+        //        L    d   .   _   F   <   >   =   +   -   *   /   '   \n   ;   :   ,  (    )   {   }  ot   !  BL  $
+        // 	  	  0    1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24
+        /*0*/    { 6,   1,  9, -1,  6,  3,  2,  5,  0,  0,  0,  0,  7,  0,  0,  0,  0,  0,  0,  0,  0, -1, -1,  0,  0  },
+        /*1*/    { 0,   1, 10,  0,  0,  0,  0,  0,  0,	0,  0,  0,  0,	0,  0,  0,  0,	0,  0,  0,  0,	0,  0,  0,  0  },
+        /*2*/    { 0,   0,	0,  0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
+        /*3*/    { 0,   0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
+        /*4*/    { 4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	0,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4  },
+        /*5*/    { 0,   0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
+        /*6*/    { 6,	6,	0,	6,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
+        /*7*/    { 7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	8,	0,	p,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7  },
+        /*8*/    { 7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	8,	0,	8,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7,	7  },
+        /*9*/    {-1,  10, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  },
+        /*10*/   { 0,  10,  0,	0, 11,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  },
+        /*11*/   {-1,  13, -1, -1, -1, -1, -1, -1, 12, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  },
+        /*12*/   {-1,  13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  },
+        /*13*/   { 0,  13,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0  };
+
+    }
+
+
+    public int getEstado(int estado, int simbolo){
+        return matriz[estado][simbolo];
+    }
+
+    public int getColCaracter(Character caracter){
+
+        if (caracter == null)
+            return -1;
+
+        if (caracter == 'F')
+            return 4;
+
+        if (Character.isLetter(caracter))
+            return 0;
+
+        if (Character.isDigit(caracter))
+            return 1;
+
+        switch (caracter) {
+            case '.': return 2;
+            case '_': return 3;
+            case '<': return 5;
+            case '>': return 6;
+            case '=': return 7;
+            case '+': return 8;
+            case '-': return 9;
+            case '*': return 10;
+            case '/': return 11;
+            case 39 : return 12;   // comilla simple (ASCII)
+            case '\n': return 13;
+            case ';': return 14;
+            case ':': return 15;
+            case ',': return 16;
+            case '(': return 17;
+            case ')': return 18;
+            case '{': return 19;
+            case '}': return 20;
+            case '!': return 22;
+            case 9: return 23;      // Tabulación (ASCII)
+            case 32: return 23;     // Espacio en blanco (ASCII)
+            case '$': return 24;
+        }
+
+        return 21; //Otros simbolos
+
+    }
+
+
+
+
+}
